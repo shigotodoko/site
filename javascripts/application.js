093c7d6cf2e15f561e4b9ae9483d910352802578
Kleber Shimabuku <klebershimabuku@gmail.com>
2015-08-19 00:26:11 -0300

    updating js

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,11 +1,34 @@
 $(document).ready(function() {
 
-  App = new Object();
+  App = {};
+
   App.displayProvinceName = $("#filter-by-province-name");
 
+  App.SearchForm = {
+    setDisplayText: function(provinceName) {
+      var $label = $('span.province-name');
+      $label.text(provinceName);
+    },
+    clearSelected: function() {
+      var selecteds = document.querySelectorAll('.province-selected');
+
+      $.each(selecteds, function(index, selected) {
+        selected.classList.remove('province-selected');
+      });
+    }
+  }
+
   var mouseOverHandler = function() {
-    var provinceName = $(this).data('province-name');
+    var provinceName  = $(this).data('province-name');
+    var $label        = $('span.province-name');
     App.displayProvinceName.val(provinceName).selected;
+
+    // search for all provinces with class 'province-selected' and remove its class.
+    App.SearchForm.clearSelected();
+
+    // Display the province name on bottom of map.
+    // $label.text(provinceName);
+    App.SearchForm.setDisplayText(provinceName);
   };
 
   var mouseOutHandler = function() {
@@ -14,5 +37,18 @@ $(document).ready(function() {
 
   $("[id^='SVG']").each(function() {
     $(this).hover(mouseOverHandler, mouseOutHandler);
+  });
+
+  App.displayProvinceName.on('change', function() {
+    var provinceName = $(this).val();
+    // search for all provinces with class 'province-selected' and remove its class.
+    App.SearchForm.clearSelected();
+
+    // highlight the province block in map
+    provinceMap = document.querySelector('[data-province-name="' + provinceName + '"]');
+    provinceMap.setAttribute('class', 'province-selected');
+
+    // Display the province selected name on label.
+    App.SearchForm.setDisplayText(provinceName);
   });
 });
